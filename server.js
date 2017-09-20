cd96aeb5a31556345a80e4b9bc5345a154160ae4
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-09-20 17:50:55 +0200

    set up express app and test routes

--- server.js
+++ server.js
@@ -6,23 +6,38 @@ const cors = require('cors');
 
 const port = process.env.PORT || 8000;
 
+
+
 //configure app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//API ROUTES
+//SET UP EXPRESS APP & ROUTES
+
+
 const router = express.Router(); //get instance of express router
-router.get('/', function(req,res) {
-  res.json({ message: 'welcome to our API :)'});
+
+// router.use(function(req, res, next) {
+//   console.log('making request to router');
+//   next();
+// });
+
+router.get('/', function(req, res) {
+  res.json({ message: 'welcome to our api!'});
 });
 
 app.use('/api', router);
+
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalurl + 'not found'})
+})
 
 //STARTING THE SERVER
 app.listen(port);
 console.log(`Application server listening on port ${port}`);
 
 //DATABASE SETUP
+// mongoose.Promise = global.Promise;
 const mongouri = process.env.PROD_MONGODB || 'mongodb://localhost/evaqaidapi';
 
 mongoose.connect(mongouri, function(err, res) {
